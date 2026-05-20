609dd48eb23cd169b70b6c8a578512488e6e29d1
woensug-choi <woensug.choi@gmail.com>
2026-05-20 21:36:46 +0900

    Mod Page

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '685f1ec60eef40cdbf217e7b91518927',
+  rootNotionPageId: '317661362ab2816ab875f2c5f69dc488',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
